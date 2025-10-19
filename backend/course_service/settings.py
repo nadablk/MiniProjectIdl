f6eeddc27bf752fb32be2d18e88c14c26f12da10
nadablk <boulakrounenada@gmail.com>
2025-10-19 23:31:23 +0100

    last

--- backend/course_service/settings.py
+++ backend/course_service/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 # Add these CORS settings
 CORS_ALLOWED_ORIGINS = [
