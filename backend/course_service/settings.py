d4db36241678c12faa22080e8deecd106d755678
nadablk <boulakrounenada@gmail.com>
2025-11-14 19:57:44 +0100

    settings fixing

--- backend/course_service/settings.py
+++ backend/course_service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%v@yqcl8+p-wpq)#%&=9$$i&5(biho5czsu6y4cft+-aysiybh'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -107,8 +110,7 @@ WSGI_APPLICATION = 'course_service.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import dj_database_url
-from decouple import config
+
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL'),
