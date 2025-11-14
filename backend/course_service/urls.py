ea589181748be94ea0743f86e3c4340f5101a612
nadablk <boulakrounenada@gmail.com>
2025-11-14 22:28:53 +0100

    Add simple health check endpoint

--- backend/course_service/urls.py
+++ backend/course_service/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from courses.schema import schema
 from graphene_django.views import GraphQLView
+
+def health_check(request):
+    return JsonResponse({'status': 'ok', 'service': 'Django Backend'})
+
 urlpatterns = [
+    path('health/', health_check),
     path('admin/', admin.site.urls),
-    path('api/', include('courses.urls')),  # Ajouter cette ligne
+    path('api/', include('courses.urls')),
     path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
